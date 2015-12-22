bc14ce3e8f0451069f2a43663c64cb07adb91f97
grzegorz2047 <grzegorz2047@gmail.com>
2015-12-22 02:51:42 +0100

    I added simple chat formatting using chat plugin.

--- src/pl/grzegorz2047/survivalcg/listeners/PlayerChatListeners.java
+++ src/pl/grzegorz2047/survivalcg/listeners/PlayerChatListeners.java
@@ -14,29 +14,29 @@ public class PlayerChatListeners implements Listener {
 
     private final SurvivalCG plugin;
 
-    public PlayerChatListeners(SurvivalCG plugin){
+    public PlayerChatListeners(SurvivalCG plugin) {
         this.plugin = plugin;
     }
 
     @EventHandler
-    public void handleEvent(AsyncPlayerChatEvent event) {
-        if(event.isCancelled()) {
+    public void onChat(AsyncPlayerChatEvent event) {
+        if (event.isCancelled()) {
             return;
         }
         Player p = event.getPlayer();
         SurvUser user = plugin.getPlayers().getUsers().get(p.getName());
         String message = event.getMessage();
 
-        if(event.getFormat().contains("%TAG%")) {
+        if (event.getFormat().contains("&GUILD_TAG&")) {
             String tag = user.getGroup();
-            event.setFormat(event.getFormat().replace("%TAG%", tag));
+            String points = String.valueOf(user.getPoints());
+
+            event.setFormat(event.getFormat().replace("&GUILD_TAG&", tag));
+            if(event.getFormat().contains("&points&")){
+                event.setFormat(event.getFormat().replace("&points&", points));
+            }
         }
     }
-
-
-
-
-
 
 
 }
